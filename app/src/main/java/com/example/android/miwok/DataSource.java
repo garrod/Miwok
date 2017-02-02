e0fe76292de6befc5fccdb773e7056cdedd15a95
Krzysztof Nawrocki <krzysztof.nawrocki79@gmail.com>
2017-02-02 21:47:48 +0100

    dodanie plikow

---
+++ app/src/main/java/com/example/android/miwok/DataSource.java
@@ -0,0 +1,19 @@
+package com.example.android.miwok;
+
+/**
+ * Created by krzysztof on 02.02.2017.
+ */
+
+public final class DataSource {
+
+    public static String[] arrayNumbers = {"one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourtheen","fifteen"};
+
+//    public String[] getArrayNumbers() {
+//        return arrayNumbers;
+//    }
+
+//    public void setArrayNumbers(String[] arrayNumbers) {
+//        this.arrayNumbers = arrayNumbers;
+//    }
+
+}
